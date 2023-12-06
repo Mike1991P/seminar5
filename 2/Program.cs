40c5364c21c9a0db0c5216e1318b0a745de8d455
MIKE <kpocobok.muxa@mail.ru>
2023-12-06 07:59:26 +0300

    добавил решение 2ой задачи

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿  var array = new int[4, 4]
+            { { 0, 5, 9, 2 },
+              { 2, 7, 6, -1 },
+              { 1, 1, 0, 2 },
+              { -2, 9, 2, 8 }
+            };
+            for(int i = 0; i < array.GetLength(1); i++)
+            {
+                var tmp = array[3, i];
+                array[3, i] = array[0, i];
+                array[0, i] = tmp;
+            }
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.Write ("Нажмите любую клавишу");
+            Console.ReadKey(true);
